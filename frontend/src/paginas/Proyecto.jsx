732fd403d431258591a5550bd2ea58b8a689a96c
Ivan <ivanbrazas@gmail.com>
2022-12-09 08:38:46 -0300

    before tareas

--- frontend/src/paginas/Proyecto.jsx
+++ frontend/src/paginas/Proyecto.jsx
@@ -45,6 +45,22 @@ const Proyecto = () => {
           <h2>Editar</h2>
         </Link>
       </div>
+
+      <button className="bg-sky-500 text-white flex rounded-md p-3 mt-3 gap-1 uppercase font-semibold  ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <h2>Nueva Tarea</h2>
+      </button>
     </div>
   );
 };
